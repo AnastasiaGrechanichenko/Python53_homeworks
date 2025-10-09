9e26e1d11fd0223d79bf285236ceb6bc594d423b
Anastasia Grechanichenko <anastasiagrecanicenko204@gmail.com>
2025-10-09 07:39:01 +0300

    фигуры

---
+++ вложенные циклы.py
@@ -0,0 +1,24 @@
+#1
+number = int(input("Введите размер: "))
+for i in range(number):
+    spaces = '  ' * i
+    stars = '* ' * (2 * (number - i) - 1)
+    print(spaces + stars)
+
+
+for i in range(number - 2, -1, -1):
+    spaces = '  ' * i
+    stars = '* ' * (2 * (number - i) - 1)
+    print(spaces + stars)
+
+
+# #3
+n = int(input("Введите размер: "))
+for i in range(1, n, 2):
+    i = ' ' * ((n - i)//2)  + '*' * i
+    print(i)
+for i in range(n, 0, -2):
+    i = ' ' * ((n - i)//2)  + '*' * i
+    print(i)
+
+
